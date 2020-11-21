fe017b6d1e32ad766677b915c27dda0a9d2b4beb
AkizoraRead-Git <akizora0322@gmail.com>
2020-11-21 14:30:34 +0900

    11/21 14:29 操作関連 Ver1.01
    ・上下左右の入力を数値で受け取れるメソッド追加

---
+++ MemberData/0408Karako/1121/操作関連/InputBase.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 11/21 12:50 更新
+//-------------------------------------------------------------------------------
+/// <summary>
+/// 入力基礎クラス
+/// </summary>
+public abstract class InputBase
+{
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// アクションボタンを押した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool ActionKeyDown();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// アクションボタンを押している瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool ActionKeyOn();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// アクションボタンを離した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool ActionKeyUp();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// メニュー起動ボタン
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool MenuKeyDown();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// キャンセルボタン
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool CanselKeyDown();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 上入力を押した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool UpKeyDown();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 上入力を押している間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool UpKeyOn();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 上入力を離した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool UpKeyUp();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 下入力を押した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool DownKeyDown();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 下入力を押している間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool DownKeyOn();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 下入力を離した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool DownKeyUp();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 右入力を押した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool RightKeyDown();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 右入力を押している間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool RightKeyOn();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 右入力を離した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool RightKeyUp();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 左入力を押した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool LeftKeyDown();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 左入力を押している間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool LeftKeyOn();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 左入力を離した瞬間
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool LeftKeyUp();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 垂直方向の入力値
+	/// </summary>
+	/// <returns></returns>
+	abstract public float AxisVertical();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// 水平方向の入力値
+	/// </summary>
+	/// <returns></returns>
+	abstract public float AxisHorizontal();
+	//-------------------------------------------------------------------------------
+	/// <summary>
+	/// ジャンプ入力
+	/// </summary>
+	/// <returns></returns>
+	abstract public bool JumpKeyDown();
+	//-------------------------------------------------------------------------------
+}
---
+++ MemberData/0408Karako/1121/操作関連/InputController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//-------------------------------------------------------------------------------
+/// <summary>
+/// コントローラー入力
+/// </summary>
+public class InputController : InputBase
+{
+	public override bool ActionKeyDown()
+	{
+		return false;
+	}
+
+	public override bool ActionKeyOn()
+	{
+		return false;
+	}
+
+	public override bool ActionKeyUp()
+	{
+		return false;
+	}
+
+	public override bool CanselKeyDown()
+	{
+		return false;
+	}
+
+	public override bool DownKeyDown()
+	{
+		return false;
+	}
+
+	public override bool DownKeyOn()
+	{
+		return false;
+	}
+
+	public override bool DownKeyUp()
+	{
+		return false;
+	}
+
+	public override bool JumpKeyDown()
+	{
+		return false;
+	}
+
+	public override bool LeftKeyDown()
+	{
+		return false;
+	}
+
+	public override bool LeftKeyOn()
+	{
+		return false;
+	}
+
+	public override bool LeftKeyUp()
+	{
+		return false;
+	}
+
+	public override bool MenuKeyDown()
+	{
+		return false;
+	}
+
+	public override bool RightKeyDown()
+	{
+		return false;
+	}
+
+	public override bool RightKeyOn()
+	{
+		return false;
+	}
+
+	public override bool RightKeyUp()
+	{
+		return false;
+	}
+
+	public override bool UpKeyDown()
+	{
+		return false;
+	}
+
+	public override bool UpKeyOn()
+	{
+		return false;
+	}
+
+	public override bool UpKeyUp()
+	{
+		return false;
+	}
+
+	public override float AxisVertical()
+	{
+		return 0.0f;
+	}
+
+	public override float AxisHorizontal()
+	{
+		return 0.0f;
+	}
+}
