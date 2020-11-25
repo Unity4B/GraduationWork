1a858010a9a88e7484a5662db2d6b6e0202d775d
AkizoraRead-Git <akizora0322@gmail.com>
2020-11-25 13:34:24 +0900

    11/25 13:34 プレイデータ
    ・プレイデータクラス

---
+++ MainData/Scripts/必須クラス/PlayData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//---------------------------------------------
+/// <summary>
+/// プレイデータ
+/// </summary>
+public class PlayData : SingletonClass<PlayData>
+{
+    //---------------------------------------------
+    /// <summary>
+    /// アイテムを持てる量
+    /// </summary>
+    [Header("持てるアイテムの量"),SerializeField] int hasItemValue = 0;
+    /// <summary>
+    /// 所持アイテムの番号
+    /// </summary>
+    public int[] itemNums;
+    /// <summary>
+    /// 所持金
+    /// </summary>
+    public int money;
+    /// <summary>
+    /// アイテムをゲットしたことがあるか
+    /// </summary>
+    public bool[] isGotItems;
+    //---------------------------------------------
+    void Start()
+    {
+        //ItemManagerのAwake後に呼び出さないとエラー?
+
+        //初期化
+        this.itemNums = new int[this.hasItemValue];
+        this.money = 0;
+        //アイテムの数だけ生成
+        this.isGotItems = new bool[ItemManager.Instance.itemList.Count];
+
+
+        //セーブデータ読み込み
+    }
+
+}
---
+++ MainData/Scripts/必須クラス/SingletonClass.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//---------------------------------------------
+/// <summary>
+/// シングルトンクラス
+/// </summary>
+public abstract class SingletonClass<T> : MonoBehaviour where T : MonoBehaviour
+{
+    /// <summary>
+    /// 生成されたクラス
+    /// </summary>
+    private static T instance;
+
+    //プロパティ
+    /// <summary>
+    /// 生成されたクラス
+    /// </summary>
+    public static T Instance
+    {
+        //戻り値
+        get
+        {
+            if (instance == null)
+            {
+                Type t = typeof(T);
+
+                instance = (T)FindObjectOfType(t);
+
+                if (instance == null)
+                {
+                    Debug.LogError(t.ToString() + "をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return instance;
+        }
+    }
+    //---------------------------------------------
+    virtual protected void Awake()
+    {
+        //他のGameObjectにアタッチされているか調べる
+        //アタッチされている場合
+        if(CheckInstance())
+        {
+            //アタッチされているのが自分以外なら、自分を破壊
+            if(instance != this) { Destroy(this); }
+
+        }
+        //アタッチされていない場合
+        else
+        {
+            //生成
+            instance = this as T;
+
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    //---------------------------------------------
+    /// <summary>
+    /// 他のGameObjectにアタッチされているか調べる
+    /// </summary>
+    /// <returns></returns>
+    protected bool CheckInstance()
+    {
+        if (instance == null)   { return false; }
+        else                    { return true; }
+    }
+    //---------------------------------------------
+}
